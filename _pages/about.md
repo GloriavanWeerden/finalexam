2d6b6004a058b202b8982885b4b4efd470ee26cd
Gloria van Weerden <gloria.vanweerden@gmail.com>
2021-04-27 19:42:58 -0400

    My name

--- _pages/about.md
+++ _pages/about.md
@@ -1,3 +1,4 @@
 # About
 
 About page here.
+https://unsplash.com/photos/GTYU1zvhv3U/download?force=true&w=1920
--- about.js
+++ about.js
@@ -2,7 +2,7 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"About", sName:"Gloria v"});
+        super({title:"About", sName:"Gloria van Weerden"});
     }
     render(sPage) {  
         return `
